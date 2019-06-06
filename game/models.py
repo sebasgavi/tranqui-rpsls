1b58f9184d75cf9dcb25b294e206e3d6fe0c9c9d
Sebastián Gaviria <sebastiangaviria@outlook.com>
2019-06-06 13:09:42 -0500

    Game model and migration

--- game/models.py
+++ game/models.py
@@ -5,3 +5,11 @@ class Player(models.Model):
     name = models.CharField(max_length=30)
     def __str__(self):
         return self.name
+
+
+class Game(models.Model):
+    player_a = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_a')
+    player_b = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_b', blank=True)
+    current_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='current_player', blank=True)
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='winner', blank=True)
+    steps = models.CharField(max_length=1000)
